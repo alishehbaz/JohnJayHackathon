40059c5c26ff4e110833555233e772d0ee7c38d2
Ali Shehbaz <alishahbaz072@gmail.com>
2023-04-29 10:08:39 -0400

    add createCampaign and donateToCampaign functions

--- synthfunds/contracts/SynthFunds.sol
+++ synthfunds/contracts/SynthFunds.sol
@@ -17,4 +17,48 @@ contract MyContract {
     mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberofCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberofCampaigns];
+        require(
+            campaign.deadline < block.timestamp,
+            "You are setting a deadline in past"
+        );
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.targetAmount = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberofCampaigns++;
+        return numberofCampaigns - 1;
+    }
+
+    function donateCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(meg.sender);
+        campaign.donations.push(amount);
+
+        bool sent = payable(campaign.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    // function getDonators() {}
+
+    // function getCampaigns() {}
 }
