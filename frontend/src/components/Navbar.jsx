024386897145248cb85cb95cbe16385c78ca55fa
Ali Shehbaz <alishahbaz072@gmail.com>
2023-04-29 12:43:11 -0400

    add button

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { CustomButton } from "./";
+import { logo, menu, search, thirdweb } from "../assets";
+import { navlinks } from "../constants";
+// import { useStateContext } from "../context";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
   return <div>Navbar</div>;
 };
 
